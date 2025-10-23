5e33b16d4a08c68b01379991f0f8b0dda5bf7e6c
f <f@ahost.win>
2025-10-22 23:12:08 -0700

    fixed sudo block

--- bin/test.sh
+++ bin/test.sh
@@ -6,6 +6,9 @@
 
 filedatetime="$(date +%Y-%m-%d_%H-%M-%S)"
 
-echo "$filedatetime" | tee -a /tmp/test.log | sudo tee -a /var/log/test.log
+echo "$filedatetime" | tee -a /tmp/test.log
 
+if [ "$EUID" -eq 0 ];  then
+echo "$filedatetime" | sudo tee -a /var/log/test.log
+fi
 
